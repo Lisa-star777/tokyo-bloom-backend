99ef60980b6298a84d0eec7dfe61309583e1b3d8
Lisa-star777 <aliskolota@mail.ru>
2026-05-06 17:55:16 +1000

    Update Kernel.php
    111

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -6,14 +6,13 @@ use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
 {
-    protected $middleware = [
-        \App\Http\Middleware\Cors::class,
-        \App\Http\Middleware\TrustProxies::class,
-        \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
-        \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
-        \App\Http\Middleware\TrimStrings::class,
-        \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-    ];
+protected $middleware = [
+    \App\Http\Middleware\TrustProxies::class,
+    \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
+    \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
+    \App\Http\Middleware\TrimStrings::class,
+    \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+];
 
     protected $middlewareGroups = [
         'web' => [
