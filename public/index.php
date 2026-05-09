7d284fb76c31c695954a116c6a0c11dc5679e99b
Lisa-star777 <aliskolota@mail.ru>
2026-05-09 14:08:53 +1000

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,29 +1,5 @@
 <?php
 
-// ===== ДОБАВЛЯЕМ CORS ЗАГОЛОВКИ =====
-$allowedOrigins = [
-    'https://tokyo-bloom.onrender.com',
-    'http://localhost:5173',
-];
-
-$origin = $_SERVER['HTTP_ORIGIN'] ?? '';
-
-if (in_array($origin, $allowedOrigins)) {
-    header('Access-Control-Allow-Origin: ' . $origin);
-}
-
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS, PATCH');
-header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, Accept, X-XSRF-TOKEN');
-header('Access-Control-Allow-Credentials: true');
-header('Access-Control-Max-Age: 86400');
-
-// Обработка preflight (OPTIONS) запросов
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(204);
-    exit();
-}
-// ===== КОНЕЦ CORS =====
-
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
