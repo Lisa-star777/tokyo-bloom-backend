b85c757ef9b130cb090d8d6d6a0ac95c1f723fbb
Lisa-star777 <aliskolota@mail.ru>
2026-05-11 20:48:29 +1000

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+
+public function index()
+{
+    $products = Cache::remember('products', 600, function () {
+        return Product::orderBy('id')->get();
+    });
+    return response()->json($products);
+}
 
 class ProductController extends Controller
 {
